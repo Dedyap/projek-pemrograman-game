02041ced15ec9e8c9f01983e89abc20c897a8cff
Dedyap <91074131+Dedyap@users.noreply.github.com>
2021-11-16 01:28:01 +0700

    Add files via upload

---
+++ admin/about.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!--//meta tags ends here-->
+     <!--booststrap-->
+     <link href="../assets/css1/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+     <!--//booststrap end-->
+     <!-- font-awesome icons -->
+     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+     <!-- //font-awesome icons -->
+     <!-- Nav-CSS -->
+     <link href="../assets/css1/nav.css" rel="stylesheet" type="text/css" media="all" />
+     <script src="../assets/js/modernizr.custom.js"></script>
+     <!-- //Nav-CSS -->
+     <!-- banner -->
+     <link rel="stylesheet" type="text/css" href="../assets/css1/uncover.css" />
+     <!--//banner -->
+     <!--stylesheets-->
+     <link href="../assets/css1/style.css" rel='stylesheet' type='text/css' media="all">
+     <!-- <link href="css/style.css" rel='stylesheet' type='text/css' media="all"> -->
+
+
+     <!--//stylesheets-->
+     <link href="//fonts.googleapis.com/css?family=Cinzel+Decorative:400,700" rel="stylesheet">
+     <link href="//fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
+     <link href="//fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
+     <title>About</title>
+</head>
+
+<body>
+     <div class="header-outs" id="about">
+          <div class="header-w3layouts">
+               <div class="container">
+                    <nav class="navbar navbar-light " style="background-color: tranparant;">
+                         <a class="navbar-brand" href="index.php">
+                              <img src="../assets/img/cok1.png" width="50" height="50" alt="">
+                         </a>
+                         <div class="right-side">
+                              <p>
+                                   <button id="trigger-overlay" type="button">
+                                        <span class="fa fa-bars" aria-hidden="true"></span>
+                                   </button>
+                              </p>
+                         </div>
+                         <!-- open/close -->
+                         <div class="overlay overlay-hugeinc">
+                              <button type="button" class="overlay-close">Close</button>
+                              <nav>
+                                   <ul>
+                                        <li><a href="index.php">Home</a></li>
+                                        <li><a href="score.php">Leaderboard</a></li>
+                                        <li><a href="about.php">About</a></li>
+                                        <li><a href="contact.php">Contact</a></li>
+                                   </ul>
+                              </nav>
+                         </div>
+
+                    </nav>
+               </div>
+          </div>
+          <div class="clearfix"> </div>
+     </div>
+</body>
+<div class="slides text-center">
+     <div class="slide slide--current one-img ">
+          <div class="slider-up">
+               <div id="kontenku">
+                    <div class="row">
+                         <div class="col-sm-12 mt-3">
+                              <h1>Let's Play Quiz!</h1>
+                         </div>
+                         <div class="col-sm-12 my-4">
+                              <img src="../assets/img/59893.png" class="img-fluid" width="500" height="500">
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <div class="slide__img">
+          </div>
+     </div>
+
+</div>
+
+<!-- js working-->
+<script src='../assets/js/jquery-2.2.3.min.js'></script>
+<!--//js working-->
+<!-- For-Banner -->
+<script src="../assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="../assets/js/anime.min.js"></script>
+<script src="../assets/js/uncover.js"></script>
+<script src="../assets/js/demo1.js"></script>
+<!-- //For-Banner -->
+<!--nav menu-->
+<script src="../assets/js/classie.js"></script>
+<script src="../assets/js/demonav.js"></script>
+<!-- //nav menu-->
+<!-- bootstrap working-->
+<script src="../assets/js/bootstrap.min.js"></script>
+<!-- // bootstrap working-->
+
+</html>
